fb82e000f0df1a98f1cd92ef3f185250aa6e938c
hoangtmm <hoangtm06@gmail.com>
2025-07-05 20:20:58 +0700

    Fix error when deploy

--- MisubishiApi/Program.cs
+++ MisubishiApi/Program.cs
@@ -1,10 +1,8 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Misubishi.BLL.Services;
-using System.Text;
 using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add controllers
@@ -15,23 +13,8 @@ builder.Services.AddControllers()
       opt.JsonSerializerOptions.MaxDepth = 64;
   });
 builder.Services.AddTransient<EmailService>();
-// JWT authentication
-var key = Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]);
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.RequireHttpsMetadata = false;
-        options.SaveToken = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(key)
-        };
-    });
 
-builder.Services.AddAuthorization();
+// CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", builder =>
@@ -40,28 +23,12 @@ builder.Services.AddCors(options =>
 
 // Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Enter 'Bearer' [space] and then your token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme { Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" } }, new string[] { }
-        }
-    });
-});
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.UseRouting();
 app.UseCors("AllowAll");
 app.UseSwagger();
 app.UseSwaggerUI();
-app.UseAuthentication();
-app.UseAuthorization();
 app.MapControllers();
 app.Run();
